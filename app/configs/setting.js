4d42d5149f31e57a5f62ce3839badd6ff0314022
boynuibon3s <boynuibon3s@gmail.com>
2023-08-25 16:09:08 +0700

    update ignored files

--- app/configs/setting.js
+++ app/configs/setting.js
@@ -7,8 +7,8 @@ module.exports = {
         facebook: '/',
         instagram: '/'
     },
-    about:` Xin chào, Nhóm mình là nhóm 7. </br>
-    Đây là project website tin tức của nhóm, nếu có sai sót gì vui lòng liên hệ.</br>
+    about:` Xin chào. </br>
+    Đây là project website tin tức của mình, nếu có sai sót gì vui lòng liên hệ.</br>
     Trân trọng cám ơn.`,
     aboutShop:`PAVSHOP là 1 project nodejs về trang web bán đồng hồ, nó vẫn còn trong giai đoạn thử nghiệm và phát triển, nếu có gì không rõ hoặc điều cần cải tiến, xin hãy trực tiếp email đến chủ web </br>
     Trân trọng cám ơn.`
